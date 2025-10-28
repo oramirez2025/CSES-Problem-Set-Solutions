646bec8e08e90d750b7c424bd1acfb156a3d7d0b
oramirez2025 <oramirez10102@gmail.com>
2025-10-28 12:08:56 -0400

    Simplified logic for reptitions

--- Introductory_Problems/reptitions.cpp
+++ Introductory_Problems/reptitions.cpp
@@ -1,34 +1,16 @@
 #include <iostream> 
-#include <map> 
-#include <string> 
-
 using namespace std;
 
 int main() {
-    map<char, int> longest;
     string s; cin >> s;
-    char currentLetter = '\0';
-    int currentCount = 0;
-    for (char c : s) {
-        if (currentLetter == '\0') {
-            currentLetter = c;
-            currentCount++;
-            longest[currentLetter] = max(currentCount, longest[currentLetter]);
-        } else {
-            if (currentLetter != c) {
-                currentLetter = c;
-                currentCount = 1;
-            } else {
-                currentCount++;
-                longest[currentLetter] = max(currentCount, longest[currentLetter]);
-            }
+    int n = s.size();
+    int curr,res = 1;
+    for (int i = 1; i < n; ++i) {
+        if (s[i] != s[i - 1]) {
+            curr = 0;
         }
+        curr++;
+        res = max(res, curr);
     }
-    int bestCount = 0;
-    for (auto &[k,v] : longest) {
-        if (v > bestCount) {
-            bestCount = v;
-        }
-    }
-    cout << bestCount << endl;
+    cout << res << endl;
 }

3a6521c3091e498f421a0270f02742691708d8e0
Oswaldo <oramirez10102@gmail.com>
2025-11-19 00:58:51 -0500

    Completed digit queries

--- Introductory_Problems/digit_queries.cpp
+++ Introductory_Problems/digit_queries.cpp
@@ -1,57 +1,35 @@
 #include <iostream>
+#include <cmath>
+#include <tgmath.h>
 
 typedef long long ll;
-/*
-// so we have to find the digit at the kth index in this infinite string
-// Well, we know that the single digits live in the interval [0,8]
-// then, the double digits live in the interval [9, 188]
-// the triple digits live in the interval [189, 2618]
-//                                         100  999
-// the quadruple digits live in the interval [2619, 31778]
-//                                            1000, 9999
-// okay, let's try to generalize this??? 
-// suppose we're looking at the dth digits so we're thinking about the numbers 
-// in the [10^{d - 1}, 9^{d}] so 10..0 - 9...9
-// cool, so I found the function for the starting index: 
-// f(1) = 9, f(d) = f(d - 1) + (d * 10) * (9^{d - 1}), where d >= 2
-// to find the right endpoint we do 
-// (f(d) + ((d + 1) * 10) * (9^{d})) - 1
-// come back to this later lol 
-*/
+using namespace std;
 
-void solve(ll k) {
-    if (0 <= k && k <= 8) {
-        NULL;
+ll powl(ll base, ll power) {
+    ll res = 1;
+    while (power--) res *= base;
+    return res;
+}
+
+void solve() {
+    ll k; cin >> k;
+    ll c = 1;
+    while (k - c*(9LL * powl(10,c - 1)) > 0) {
+        k -= c*(9LL * powl(10,c - 1));
+        c++;
+    }
+    ll quo = k / c;
+    ll rem = k % c;
+    if (rem == 0) {
+        cout << ((powl(10,c - 1) + quo - 1) % 10) << '\n';
     } else {
-        ll d = 2;
-        ll l = 9, r = 188;
-        while (!(l <= k && k <= r)) {
-            d++;
-            l += (d * 10) * (pow(9, d - 1));
-            r = (l + ((d + 1) * 10) * (pow(9, d))) - 1;
-        }
-        // cool, now we know l <= k <= r but this is too big to determine??? we should go into the sub intervals
-        ll rr = l + (d * 10);
-        ll number = pow(10, d - 1);
-        while (!(l <= k && k <= rr)) {
-            number += 10;
-            l = rr;
-            rr = l + (d * 10);
-        }
-        rr--;
-        std::cout << "l = " << l << " rr = " << rr << " d = " << d << " number = " << number << '\n';
-        // rrr = 
-        for (int i = number; i < number + 10; ++i) {
-
-        }
+        cout << (((powl(10,c - 1) + quo) / (powl(10,c-rem))) % 10) << '\n';
     }
 }
 
-
 int main() {
-    int queries; std::cin >> queries;
-    while (queries--) {
-        ll k; std::cin >> k;
-        solve(k);
+    int q; cin >> q;
+    while (q--) {
+        solve();
     }
 }
